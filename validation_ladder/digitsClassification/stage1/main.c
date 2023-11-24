7695cd55a89cada65fa44a34f4b5a2bb7908a45e
imsamrth <samarthbnsl@gmail.com>
2023-11-24 16:36:04 +0530

    changed config to 1*1

--- validation_ladder/digitsClassification/stage1/main.c
+++ validation_ladder/digitsClassification/stage1/main.c
@@ -5,12 +5,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
-#ifdef USE_CORTOS
 #include <cortos.h>
-#else
-#include <core_portme.h>
-#endif
-#include <thread_channel.h>
 #include <ajit_access_routines.h>
 #include "include/matrix.h"
 #include "include/ops.h"
@@ -19,20 +14,20 @@
 #include "include/nn.h"
 #include "include/net.h"
 
-volatile ThreadChannel volatile tc;
+//volatile ThreadChannel volatile tc;
 
-void setup()
-{
-	__ajit_serial_configure__(1,0,0);
-	__ajit_serial_set_baudrate__ (115200, 80000000);
-	__ajit_serial_set_uart_reset__ (0);
+// void setup()
+// {
+// 	__ajit_serial_configure__(1,0,0);
+// 	__ajit_serial_set_baudrate__ (115200, 80000000);
+// 	__ajit_serial_set_uart_reset__ (0);
 
-	PRINTF("Init channel.\n");
-	// initialize scoreboard.
-	initChannel(&tc, 1);
+// 	PRINTF("Init channel.\n");
+// 	// initialize scoreboard.
+// 	initChannel(&tc, 1);
 
-	CORTOS_DEBUG("initChannel %d: status=%d.\n", tc.id, tc.status);
-}
+// 	CORTOS_DEBUG("initChannel %d: status=%d.\n", tc.id, tc.status);
+// }
 
 
 int main_00()
@@ -52,30 +47,29 @@ int main_00()
 
 	return(1);
 }
-
-int main_01 ()
-{
-	void (*__fn) (void*);
-	void *__arg;
-
-	CORTOS_DEBUG("Entered main_01\n");
-
-	while(1) 
-	{
-		while(getChannelJob(&tc, (void**) &__fn, (void**) &__arg))
-		{
-		}
-
-		if(__fn != NULL)
-		{
-			(*__fn)(__arg);
-			setChannelResponse(&tc, __arg);
-		}
-		else
-			break;
-	}
-
-	return(1);
-
-}
-
+//
+//int main_01 ()
+//{
+//	void (*__fn) (void*);
+//	void *__arg;
+//
+//	CORTOS_DEBUG("Entered main_01\n");
+//
+//	while(1) 
+//	{
+//		while(getChannelJob(&tc, (void**) &__fn, (void**) &__arg))
+//		{
+//		}
+//
+//		if(__fn != NULL)
+//		{
+//			(*__fn)(__arg);
+//			setChannelResponse(&tc, __arg);
+//		}
+//		else
+//			break;
+//	}
+//
+//	return(1);
+//
+//}
